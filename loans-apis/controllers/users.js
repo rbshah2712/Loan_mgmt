a2a59628585da911a5aa1b3eca9cc7aaefb3ccf4
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-10-11 11:15:32 -0400

    view engine approach deleted

--- loans-apis/controllers/users.js
+++ loans-apis/controllers/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const usersModel = require('../models/users.model');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendStatus(200).send();
 });
 
-module.exports = router;
